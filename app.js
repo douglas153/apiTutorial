f6177ab0c2767079bb10112e1642d9637a3790ee
Douglas Sousa <douglas.sousa@compline.com.br>
2020-02-19 16:13:04 -0300

    Conexao com Mongoose

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express'); 
 const morgan = require('morgan');
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express(); 
 
 const productRoutes = require('./api/routes/products');
 const ordersRoutes = require('./api/routes/orders')
-
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended: false})); 
 app.use(bodyParser.json());
 
+mongoose.connect('mongodb+srv://node-shop:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-apl5r.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true
+}
+)
 app.use('/products', productRoutes);
 app.use('/orders', ordersRoutes);
 
