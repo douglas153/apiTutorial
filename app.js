9c7a1772270cd77d9f3881e41a51f4063c5628d4
Douglas Sousa <douglas.sousa@compline.com.br>
2020-02-27 13:31:13 -0300

    corrigindo Cors, aceitando requisiçoes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-
+const mongoose = require('mongoose'); 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
@@ -10,18 +10,23 @@ app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === 'OPTIONS'){
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
-  }
-  next();
-});
+//Mongo Connection 
+mongoose.connect('mongodb+srv://node-shop:'+ process.env.MONGO_ATLAS_PW +'@node-rest-shop-apl5r.mongodb.net/test?retryWrites=true&w=majority', 
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+);
+mongoose.Promise = global.Promise;
+
+
+//allowing Cors 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
 // Routes which should handle requests
 app.use("/products", productRoutes);
